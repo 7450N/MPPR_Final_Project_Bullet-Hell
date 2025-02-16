dac76bd778581a1e0841831d16b2eceaa5c6454e
7450N <minthawkaung10@gmail.com>
2025-02-17 03:09:34 +0800

    pushing joseph

---
+++ Bullet Hell/Assets/Resources/Scripts/Joseph/SceneChangeOnObjectDisappear.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChangeOnObjectDisappear : MonoBehaviour
+{
+    public GameObject targetObject; // The object to monitor
+    public string sceneName;        // Scene to switch to
+
+    void Update()
+    {
+        // Check if the target object is missing (destroyed or inactive)
+        if (targetObject == null || !targetObject.activeInHierarchy)
+        {
+            ChangeScene();
+        }
+    }
+
+    void ChangeScene()
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
---
+++ Bullet Hell/Assets/Resources/Scripts/Joseph/SceneChanger.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChanger : MonoBehaviour
+{
+    public float delay = 5f; // Time in seconds before changing the scene
+    public string sceneName; // Name of the scene to load
+
+    void Start()
+    {
+        // Invoke the scene change after the specified delay
+        Invoke("ChangeScene", delay);
+    }
+
+    void ChangeScene()
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
